cfe767be8939fbd33d62ccd3ae664e4642cf7cfa
hiepnguyen <hiepnguyen8069@gmail.com>
2019-05-03 14:27:38 -0500

    Abstract class - Nelson

---
+++ webapp/backend/flaskr/Abstract_review.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class ReviewObject(ABC):
+    def __init__(self, dictionary):
+        self.id = dictionary["id"]
+        self.book_id = dictionary["book_id"]
+        self.title = dictionary["title"]
+        self.isbn = dictionary["isbn"]
+        self.author = dictionary["author"]
+        self.review_source = dictionary["review_source"]
+        self.review_content = dictionary["review_content"]
+
+    @abstractmethod
+    def find_by_id(self, book_id):
+        pass
