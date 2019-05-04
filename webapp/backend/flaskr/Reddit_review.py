a1db9416bfe2379eeb8b6fb99939d9813d01afce
michael <michael@michael>
2019-05-04 18:24:53 -0500

    Merge Hiep's code into master w/ minor tweaks

--- webapp/backend/flaskr/Reddit_review.py
+++ webapp/backend/flaskr/Reddit_review.py
@@ -14,7 +14,7 @@ from flaskr.db import get_db
 app = Flask(__name__)
 from flaskr.model import Model
 
-class BnReview(Model):
+class RedditReview(Model):
     def __init__(self, dictionary):
         self.dictionary = dictionary
         self.id = dictionary["id"]
@@ -23,7 +23,6 @@ class BnReview(Model):
         self.isbn = dictionary["isbn"]
         self.author = dictionary["author"]
         self.review_source = dictionary["review_source"]
-        self.average_rating = dictionary["average_rating"]
         self.review_author = dictionary["review_author"]
         self.review_content = dictionary["review_content"]
 
@@ -31,24 +30,24 @@ class BnReview(Model):
         with app.app_context():
             cursor = get_db().cursor()
             cursor.execute(
-                'SELECT * FROM BN WHERE BN.book_id = %s;', (book_id,)
+                'SELECT * FROM reddit WHERE reddit.book_id = %s;', (book_id,)
             )
-            BN_reviews_dictionary = cursor.fetchall()
-            BN_reviews = [];
-            for BN_review_dictionary in BN_reviews_dictionary:
-                BN_review = BnReview(BN_review_dictionary)
-                BN_reviews.append(BN_review)
-            return BN_reviews
+            reddit_reviews_dictionary = cursor.fetchall()
+            reddit_reviews = [];
+            for reddit_review_dictionary in reddit_reviews_dictionary:
+                reddit_review = RedditReview(reddit_review_dictionary)
+                reddit_reviews.append(reddit_review)
+            return reddit_reviews
 
     def find_by_isbn(isbn):
         with app.app_context():
             cursor = get_db().cursor()
             cursor.execute(
-                'SELECT * FROM BN WHERE BN.isbn = %s;', (isbn,)
+                'SELECT * FROM reddit WHERE reddit.isbn = %s;', (isbn,)
             )
-            BN_reviews_dictionary = cursor.fetchall()
-            BN_reviews = [];
-            for BN_review_dictionary in BN_reviews_dictionary:
-                BN_review = BnReview(BN_review_dictionary)
-                BN_reviews.append(BN_review)
-            return BN_reviews
+            reddit_reviews_dictionary = cursor.fetchall()
+            reddit_reviews = [];
+            for reddit_review_dictionary in reddit_reviews_dictionary:
+                reddit_review = RedditReview(reddit_review_dictionary)
+                reddit_reviews.append(reddit_review)
+            return reddit_reviews
